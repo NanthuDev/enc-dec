3a28993266184205bd2319a9b78f6b8edefc0cd5
Nantha <nanthu1593@gmail.com>
2024-08-25 23:32:29 +0800

    udpate

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 
 
 app.use('/',(req,res)=>{res.send("Test")})
+app.post('/generate',(req,res)=>{res.send("Test")})
+app.post('/verify',(req,res)=>{res.send("Test")})
+
+
 
 app.listen(3000,()=>{
     console.log("App connected at 3000")
---
+++ hotp.js
@@ -0,0 +1,7 @@
+
+const speakeasy = require("speakeasy");
+user.two_factor_temp_secret = secret.base32;
+var QRCode = require('qrcode');
+
+
+var secret = speakeasy.generateSecret();
