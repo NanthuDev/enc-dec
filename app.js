93d2100e1382a409e7048805b0447fbe47a85771
Nantha <nanthu1593@gmail.com>
2024-08-26 00:54:28 +0800

    otp

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use('/',(req,res)=>{res.send("Test")})
 app.post('/generate',(req,res)=>{res.send("Test")})
 app.post('/verify',(req,res)=>{res.send("Test")})
 
+app.post('/hotp',(req,res)=>{res.send("Test")})
+app.post('/totp',(req,res)=>{res.send("Test")})
 
 
 app.listen(3000,()=>{
