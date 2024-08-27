18eeeb8c1892799e5abbcb400dbc2cd2154c8128
Nantha <nanthu1593@gmail.com>
2024-08-27 23:24:04 +0800

    verify

--- totp.js
+++ totp.js
@@ -7,4 +7,10 @@ module.exports = {
     // Access using secret.ascii, secret.hex, or secret.base32.
     return res.send({"send": secret});
   },
+  verify: (req, res) => {
+    // Generate a secret key.
+    var secret = speakeasy.generateSecret({ length: 20 });
+    // Access using secret.ascii, secret.hex, or secret.base32.
+    return res.send({"send": secret});
+  },
 };
