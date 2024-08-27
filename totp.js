87d49fefddd37c25df1597d6140494c724d45f4b
Nantha <nanthu1593@gmail.com>
2024-08-27 23:04:58 +0800

    Update totp.js

--- totp.js
+++ totp.js
@@ -1,8 +1,10 @@
 var speakeasy = require("speakeasy");
 
-
 module.exports = {
-    generate : (req,res)=>{
-        return res.send("send")
-    }
-}
+  generate: (req, res) => {
+    // Generate a secret key.
+    var secret = speakeasy.generateSecret({ length: 20 });
+    // Access using secret.ascii, secret.hex, or secret.base32.
+    return res.send({"send": secret});
+  },
+};
